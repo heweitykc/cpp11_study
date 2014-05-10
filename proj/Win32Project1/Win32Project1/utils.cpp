8fde96e883f0e87a92582ec12cf5561fd95ca6ac
callee <heweitykc@163.com>
2014-05-10 22:30:30 +0800

    1

--- proj/Win32Project1/Win32Project1/utils.cpp
+++ proj/Win32Project1/Win32Project1/utils.cpp
@@ -1,10 +1,20 @@
 #include "stdafx.h"
 #include "utils.h"
 
+#define TXTCOUNT 2048
 static int text_cnt = 0;
+static TCHAR msg[TXTCOUNT];
+static char cmsg[TXTCOUNT];
 
-void disp(HWND hwnd, const TCHAR* str)
+void dispW(HWND hwnd, const TCHAR* str, ...)
 {
+	va_list args;
+	va_start(args, str);
+
+	ZeroMemory(msg, TXTCOUNT);
+	StringCchPrintf(msg, TXTCOUNT, str, args);
+	va_end(args);
+
 	HFONT hfont, hOldfont;
 	HDC hdc;
 
@@ -12,15 +22,22 @@ void disp(HWND hwnd, const TCHAR* str)
 	hdc = GetDC(hwnd);
 	hOldfont = (HFONT)SelectObject(hdc, hfont);
 	if (hOldfont){
-		TextOutW(hdc, 5, text_cnt * 20, str, lstrlenW(str));
+		TextOutW(hdc, 5, text_cnt * 20, msg, lstrlenW(msg));
 		SelectObject(hdc, hOldfont);
 	}
 	ReleaseDC(hwnd, hdc);
 	text_cnt++;
 }
 
-void disp(HWND hwnd, const char* str)
+void dispA(HWND hwnd, const char* str, ...)
 {
+	va_list args;
+	va_start(args, str);
+
+	ZeroMemory(cmsg, TXTCOUNT);
+	sprintf(cmsg, str, args);
+	va_end(args);
+
 	HFONT hfont, hOldfont;
 	HDC hdc;
 
@@ -28,7 +45,7 @@ void disp(HWND hwnd, const char* str)
 	hdc = GetDC(hwnd);
 	hOldfont = (HFONT)SelectObject(hdc, hfont);
 	if (hOldfont){
-		TextOutA(hdc, 5, text_cnt * 20, str, strlen(str));
+		TextOutA(hdc, 5, text_cnt * 20, cmsg, strlen(cmsg));
 		SelectObject(hdc, hOldfont);
 	}
 	ReleaseDC(hwnd, hdc);
