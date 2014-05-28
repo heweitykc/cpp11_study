63b4a76953a65351a5ae23057bfefc53aae603cd
callee <heweitykc@163.com>
2014-05-28 16:23:03 +0800

    generator

---
+++ proj/Win32Project1/Win32Project1/generator.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "generator.h"
+#include <numeric>
+
+void generator::doit()
+{
+	
+}
---
+++ proj/Win32Project1/Win32Project1/generator.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "win32base.h"
+
+class generator : public win32base
+{
+public:
+	void doit();
+};
