dd14b103511c021ce7a3fa41b3a7d622c1de2378
callee <heweitykc@163.com>
2014-06-04 18:43:19 +0800

    asd

--- proj/texturetool/texturetool/texturetool.cpp
+++ proj/texturetool/texturetool/texturetool.cpp
@@ -3,10 +3,24 @@
 
 #include "stdafx.h"
 
+using namespace std;
+
+const int ALLSIZE = 11 * 11;
+
+struct wh{
+	int w;
+	int h;
+};
+
+array<wh, ALLSIZE> allSizes;	//所有可能的尺寸
+vector<wh> outputlist;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-
+	int i = 0;
+	while (i++ < ALLSIZE){
+		allSizes[i] = 
+	}
 	return 0;
 }
 
